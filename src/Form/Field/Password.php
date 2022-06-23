aceb937a447548eccfe77fef3f6c7bbbd679af05
SB | Open-admin.org <info@open-admin.org>
2022-06-23 12:59:47 +0200

    add show password toggle

--- src/Form/Field/Password.php
+++ src/Form/Field/Password.php
@@ -4,10 +4,40 @@ namespace OpenAdmin\Admin\Form\Field;
 
 class Password extends Text
 {
+    public $showPasswordToggle = true;
+
+    public function toggleShow($set = true)
+    {
+        $this->showPasswordToggle = $set;
+    }
+
+    public function setupScript()
+    {
+        $this->script = <<<JS
+            window.togglePassword = function(ref){
+                var field = document.getElementById(ref.dataset.id);
+                if (field.type == "password"){
+                    field.type = "text";
+                    ref.classList.remove("icon-eye");
+                    ref.classList.add("icon-eye-slash");
+                }else{
+                    field.type = "password";
+                    ref.classList.remove("icon-eye-slash");
+                    ref.classList.add("icon-eye");
+                }
+            }
+        JS;
+    }
+
     public function render()
     {
         $this->prepend('<i class="icon-eye-slash fa-fw"></i>')
-            ->defaultAttribute('type', 'password');
+             ->defaultAttribute('type', 'password');
+
+        if ($this->showPasswordToggle) {
+            $this->setupScript();
+            $this->append('<i class="icon-eye fa-fw" style="cursor:pointer;margin:-5px;padding:5px;" data-id="'.$this->id.'" onclick="window.togglePassword(this)"></i>');
+        }
 
         return parent::render();
     }
