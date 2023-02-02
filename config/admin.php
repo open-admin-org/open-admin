ed0e25315621ad6d4c9c2e85a43ade34ed4a71c6
SB | Open-admin.org <info@open-admin.org>
2023-02-02 21:56:48 +0100

    fixed #37 , not a real fix but this feature is gone from open-admin

--- config/admin.php
+++ config/admin.php
@@ -284,7 +284,7 @@ return [
     | The orginal adminlte theme is not used anymore.
     |
     */
-    'skin' => 'skin-blue-light',
+    'skin' => 'your-custom-skin-class',
 
     /*
     |--------------------------------------------------------------------------
