17c22160a1867cc2e574062a4f4175f246ae3de4
Cyrix <124294361+RCyrix@users.noreply.github.com>
2023-04-01 21:55:13 +0200

    Switch field fix on form
    Added conditional logic for True/False in form 
    Changed ‘0’ to numeric value

--- src/Form/Field/SwitchField.php
+++ src/Form/Field/SwitchField.php
@@ -8,16 +8,16 @@ class SwitchField extends Field
 {
     public function prepare($value)
     {
-        if ($value == 'on' || $value == 1) {
+        if ($value == 'on' || $value == 1 || $value == true) {
             $value = 1;
-        } elseif ($value == 'off' || $value === '0') {
+        } elseif ($value == 'off' || $value == 0 || $value == false) {
             $value = 0;
         } else {
             $value = false; // nothting was set so do: false to ignore value from saving
         }
 
         return $value;
-    }
+    }s
 
     public function render()
     {
