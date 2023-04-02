bf53c096355b6008cf1b6f9cb0c1a199e220bb2a
SB | Open-admin.org <info@open-admin.org>
2023-04-03 00:01:03 +0200

    #53 -  fixedFooter is displayed for HasOne, BelongsTo, MorphIne relations - only set fixedFooter for Grid

--- src/Show/Relation.php
+++ src/Show/Relation.php
@@ -53,9 +53,9 @@ class Relation extends Field
      */
     public function __construct($name, $builder, $title = '')
     {
-        $this->name = $name;
+        $this->name    = $name;
         $this->builder = $builder;
-        $this->title = $this->formatLabel($title);
+        $this->title   = $this->formatLabel($title);
     }
 
     /**
@@ -121,10 +121,9 @@ class Relation extends Field
 
             $renderable->setName($this->name)
                 ->setTitle($this->title)
-                ->setRelation($relation);
+                ->setRelation($relation)
+                ->fixedFooter(false);
         }
-
-        $renderable->fixedFooter(false);
 
         return $renderable->render();
     }
