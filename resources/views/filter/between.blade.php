e75747aac31ef803a2ec5ea95b7f1e7cbc1b18ac
SB | Open-admin.org <info@open-admin.org>
2023-05-01 14:18:28 +0200

    add option to change default width of label / field in filters

--- resources/views/filter/between.blade.php
+++ resources/views/filter/between.blade.php
@@ -1,10 +1,12 @@
 <div class="form-group row">
-    <label class="col-sm-2 control-label">{{$label}}</label>
-    <div class="col-sm-8" style="width: 390px">
+    <label class="col-sm-{{ $cols_label }} form-label">{{ $label }}</label>
+    <div class="col-sm-{{ $cols_field }}" style="width: 390px">
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="{{$label}}" name="{{$name['start']}}" value="{{ request()->input("{$column}.start", \Illuminate\Support\Arr::get($value, 'start')) }}">
+            <input type="text" class="form-control" placeholder="{{ $label }}" name="{{ $name['start'] }}"
+                value="{{ request()->input("{$column}.start", \Illuminate\Support\Arr::get($value, 'start')) }}">
             <span class="input-group-text" style="border-left: 0; border-right: 0;">-</span>
-            <input type="text" class="form-control" placeholder="{{$label}}" name="{{$name['end']}}" value="{{ request()->input("{$column}.end", \Illuminate\Support\Arr::get($value, 'end')) }}">
+            <input type="text" class="form-control" placeholder="{{ $label }}" name="{{ $name['end'] }}"
+                value="{{ request()->input("{$column}.end", \Illuminate\Support\Arr::get($value, 'end')) }}">
         </div>
     </div>
 </div>
