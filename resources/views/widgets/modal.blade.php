9af5dc7f8df9564be86de20519f3c1878656776a
SB | Open-admin.org <info@open-admin.org>
2023-05-01 17:03:44 +0200

    - put all form rows in a container (better alignment inside modal) - make modal scrollable

--- resources/views/widgets/modal.blade.php
+++ resources/views/widgets/modal.blade.php
@@ -1,8 +1,8 @@
 <div {!! $attributes !!}>
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog  modal-dialog-scrollable modal-lg" role="document">
         <div class="modal-content" style="border-radius: 5px;">
             <div class="modal-header">
-                <h4 class="modal-title">{{$title}}</h4>
+                <h4 class="modal-title">{{ $title }}</h4>
                 <button type="button" class="btn btn-light close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -13,17 +13,17 @@
                     {!! $body !!}
                 @endif
                 @if ($show_loader)
-                <div class="loading text-center">
-                    <div class="icon-spin">
-                        <i class="icon-spinner icon-spin icon-3x icon-fw"></i>
+                    <div class="loading text-center">
+                        <div class="icon-spin">
+                            <i class="icon-spinner icon-spin icon-3x icon-fw"></i>
+                        </div>
                     </div>
-                </div>
                 @endif
             </div>
             @if (!empty($footer))
-            <div class="modal-footer">
-                {!!$footer!!}
-            </div>
+                <div class="modal-footer">
+                    {!! $footer !!}
+                </div>
             @endif
         </div>
     </div>
