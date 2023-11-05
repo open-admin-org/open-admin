01a9b8ee4dc5743faf8e3e592154a8012699e1d2
SB | Open-admin.org <info@open-admin.org>
2023-11-05 12:48:34 +0100

    fixing https://github.com/open-admin-org/open-admin/issues/71 required for stacked checkboxes

--- resources/assets/open-admin/js/helpers.js
+++ resources/assets/open-admin/js/helpers.js
@@ -2,136 +2,136 @@
 /* visual */
 /*--------------------------------------------------*/
 
-	var show = function (list,display) {
-		if (typeof(display) === 'undefined'){
-			display = "block";
-		}
-		if (!isNodeList(list)){
-			var list = [list];
-		}
-		list.forEach(elm => {
-			showElm(elm,display);
-		});
-	};
-	function showElm(elm,display){
-		if(elm.tagName == "TR"){
-			elm.style.display = "table-row";
-		}else{
-			elm.style.display = display;
-		}
+var show = function (list, display) {
+	if (typeof (display) === 'undefined') {
+		display = "block";
 	}
+	if (!isNodeList(list)) {
+		var list = [list];
+	}
+	list.forEach(elm => {
+		showElm(elm, display);
+	});
+};
+function showElm(elm, display) {
+	if (elm.tagName == "TR") {
+		elm.style.display = "table-row";
+	} else {
+		elm.style.display = display;
+	}
+}
 
-	var hide = function (list) {
-		if (!isNodeList(list)){
-			var list = [list];
-			isNodeList(list)
-		}
-		list.forEach(elm => {
+var hide = function (list) {
+	if (!isNodeList(list)) {
+		var list = [list];
+		isNodeList(list)
+	}
+	list.forEach(elm => {
+		elm.style.display = 'none';
+	});
+};
+
+var toggle = function (list) {
+	if (!isNodeList(list)) {
+		var list = [list];
+	}
+	list.forEach(elm => {
+		let calculatedStyle = window.getComputedStyle(elm).display;
+		if (calculatedStyle === 'block' || calculatedStyle === 'flex' || calculatedStyle === 'table-row') {
 			elm.style.display = 'none';
-		});
-	};
-
-	var toggle = function (list) {
-		if (!isNodeList(list)){
-			var list = [list];
+			return;
 		}
-		list.forEach(elm => {
-			let calculatedStyle = window.getComputedStyle(elm).display;
-			if (calculatedStyle === 'block' || calculatedStyle === 'flex' || calculatedStyle === 'table-row') {
-				elm.style.display = 'none';
-				return;
-			}
-			showElm(elm);
-		});
-	};
+		showElm(elm);
+	});
+};
 
 /*--------------------------------------------------*/
 /* lang function */
 /*--------------------------------------------------*/
 
-	var __ = function(trans_string){
-		return admin_lang_arr[trans_string];
-	}
+var __ = function (trans_string) {
+	return admin_lang_arr[trans_string];
+}
 
-	var trans = __;
+var trans = __;
 
 /*--------------------------------------------------*/
 /* array / object helpers */
 /*--------------------------------------------------*/
 
-	var merge_default = function(defaults,object, ...rest){
-		return Object.assign({}, defaults, object, ...rest);
-	}
+var merge_default = function (defaults, object, ...rest) {
+	return Object.assign({}, defaults, object, ...rest);
+}
 
-	var arr_remove = function(arr,elem) {
-		var indexElement = arr.findIndex(el => el == elem);
-		if (indexElement != -1)
-		  arr.splice(indexElement, 1);
-		return arr;
-	};
+var arr_remove = function (arr, elem) {
+	var indexElement = arr.findIndex(el => el == elem);
+	if (indexElement != -1)
+		arr.splice(indexElement, 1);
+	return arr;
+};
 
-	var arr_includes = function(arr,elem) {
-		var indexElement = arr.findIndex(el => el == elem);
-		return (indexElement != -1)
-	};
+var arr_includes = function (arr, elem) {
+	var indexElement = arr.findIndex(el => el == elem);
+	return (indexElement != -1)
+};
 
 /*--------------------------------------------------*/
 /* event Handlers  */
 /*--------------------------------------------------*/
 
-	function delegate(selector, handler) {
+function delegate(selector, handler) {
 
-		return function(event) {
-		  var targ = event.target;
-		  do {
+	return function (event) {
+		var targ = event.target;
+		do {
 			if (targ.matches(selector)) {
-			  handler.call(targ, event);
+				handler.call(targ, event);
 			}
-		  } while ((targ = targ.parentNode) && targ != event.currentTarget);
-		}
-	  }
+		} while ((targ = targ.parentNode) && targ != event.currentTarget);
+	}
+}
 
 /*--------------------------------------------------*/
 /* html elements */
 /*--------------------------------------------------*/
 
-	function getOuterHeigt(el) {
-		// Get the DOM Node if you pass in a string
-		el = (typeof el === 'string') ? document.querySelector(el) : el;
+function getOuterHeigt(el) {
+	// Get the DOM Node if you pass in a string
+	el = (typeof el === 'string') ? document.querySelector(el) : el;
 
-		var styles = window.getComputedStyle(el);
-		var margin = parseFloat(styles['marginTop']) +
-					 parseFloat(styles['marginBottom']);
+	var styles = window.getComputedStyle(el);
+	var margin = parseFloat(styles['marginTop']) +
+		parseFloat(styles['marginBottom']);
 
-		return Math.ceil(el.offsetHeight + margin);
-	}
+	return Math.ceil(el.offsetHeight + margin);
+}
 
-	function isNodeList(nodes) {
-		var stringRepr = Object.prototype.toString.call(nodes);
+function isNodeList(nodes) {
+	var stringRepr = Object.prototype.toString.call(nodes);
 
-		return typeof nodes === 'object' &&
-			/^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
-			(typeof nodes.length === 'number') &&
-			(nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
-	}
+	return typeof nodes === 'object' &&
+		/^\[object (HTMLCollection|NodeList|Object)\]$/.test(stringRepr) &&
+		(typeof nodes.length === 'number') &&
+		(nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0));
+}
 
-	/**
-	 * @param {String} HTML representing a single element
-	 * @return {Element}
-	 */
-	function htmlToElement(html) {
-		var template = document.createElement('template');
-		html = html.trim(); // Never return a text node of whitespace as the result
-		template.innerHTML = html;
-		return template.content.firstChild;
-	}
+/**
+ * @param {String} HTML representing a single element
+ * @return {Element}
+ */
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim(); // Never return a text node of whitespace as the result
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-	/**
-	 * @param {String} HTML representing any number of sibling elements
-	 * @return {NodeList}
-	 */
-	function htmlToElements(html) {
-		var template = document.createElement('template');
-		template.innerHTML = html;
-		return template.content.childNodes;
-	}
+/**
+ * @param {String} HTML representing any number of sibling elements
+ * @return {NodeList}
+ */
+function htmlToElements(html) {
+	var template = document.createElement('template');
+	template.innerHTML = html;
+	return template.content.childNodes;
+}
--- src/Form/Field/Checkbox.php
+++ src/Form/Field/Checkbox.php
@@ -2,6 +2,7 @@
 
 namespace OpenAdmin\Admin\Form\Field;
 
+use Admin;
 use Illuminate\Contracts\Support\Arrayable;
 
 class Checkbox extends MultipleSelect
@@ -12,6 +13,7 @@ class Checkbox extends MultipleSelect
      * @var string
      */
     protected $cascadeEvent = 'change';
+
 
     /**
      * Set options.
@@ -77,6 +79,36 @@ class Checkbox extends MultipleSelect
         return $this;
     }
 
+    public function script()
+    {
+        $elementClassSelector = $this->getElementClassSelector();
+        $requireMessage = $this->validationMessages['required'] ?? 'This field is required';
+
+        if ($this->stacked && in_array("required", $this->rules)) {
+            $script = <<<JS
+
+            document.querySelectorAll("{$elementClassSelector}").forEach((el)=>{
+                el.addEventListener('change', function(e) {
+                    if ( document.querySelectorAll("{$elementClassSelector}:checked").length == 0){
+
+                        document.querySelectorAll("{$elementClassSelector}").forEach((rel)=>{
+                            rel.setCustomValidity("{$requireMessage}");
+                        })
+                    }else{
+                        document.querySelectorAll("{$elementClassSelector}").forEach((rel)=>{
+                            rel.setCustomValidity("");
+                            rel.removeAttribute("required");
+                        })
+                    }
+                })
+            })
+        JS;
+        }
+
+        Admin::script($script);
+    }
+
+
     /**
      * {@inheritdoc}
      */
@@ -86,6 +118,8 @@ class Checkbox extends MultipleSelect
             'checked'      => $this->checked,
             'stacked'      => $this->stacked,
         ]);
+
+        $this->script();
 
         return parent::render();
     }
