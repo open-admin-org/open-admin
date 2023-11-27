5eff3c61ca49d7584f505d939c1c3ba58b70b404
EMMAN <emmanuelbarrameda1@gmail.com>
2023-11-27 22:24:24 +0800

    Tooltip added on Button Actions of Delete, Edit and Show
    Tooltip added on Button Actions of Delete, Edit and Show
    emmanpbarrameda

--- src/Grid/Actions/Show.php
+++ src/Grid/Actions/Show.php
@@ -23,4 +23,33 @@ class Show extends RowAction
     {
         return "{$this->getResource()}/{$this->getKey()}";
     }
+
+
+    /**
+     * Render row action with a tooltip.
+     *
+     * @return string
+     */
+    public function render() {
+        $linkClass = ($this->parent->getActionClass() != "OpenAdmin\Admin\Grid\Displayers\Actions\Actions") ? 'dropdown-item' : '';
+        $icon = $this->getIcon();
+
+        $tooltip = 'Show details'; //tooltip content
+
+        if ($href = $this->href()) {
+            return "<a href='{$href}' class='{$linkClass}' title='{$tooltip}'>{$icon}<span class='label'>{$this->name()}</span></a>";
+        }
+
+        $this->addScript();
+
+        $attributes = $this->formatAttributes();
+
+        return sprintf(
+            "<a data-_key='%s' href='javascript:void(0);' class='%s {$linkClass}' {$attributes} title='{$tooltip}'>{$icon}<span class='label'>%s</span></a>",
+            $this->getKey(),
+            $this->getElementClass(),
+            $this->asColumn ? $this->display($this->row($this->column->getName())) : $this->name()
+        );
+    }
+
 }
