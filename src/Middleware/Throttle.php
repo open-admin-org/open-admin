ef94d4535c724869c6ea9b85939e9287ee42f14b
EMMAN <emmanuelbarrameda1@gmail.com>
2024-01-02 04:49:44 +0800

    auth.throttle time countdown added

--- src/Middleware/Throttle.php
+++ src/Middleware/Throttle.php
@@ -37,8 +37,12 @@ class Throttle
 
     protected function getToManyAttemptsMessage()
     {
-        return Lang::has('auth.to_many_attempts')
-            ? trans('auth.to_many_attempts')
-            : 'To many attempts!';
+        $seconds = RateLimiter::availableIn('login-tries-' . Admin::guardName());
+    
+        $message = Lang::has('auth.throttle')
+            ? trans('auth.throttle', ['seconds' => $seconds])
+            : "Too many login attempts. Please try again in " . $seconds . " seconds.";
+    
+        return $message;
     }
 }
